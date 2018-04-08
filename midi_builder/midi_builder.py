801dca1728cca84ad140161964e5c287c44aa0c0
superlou <lousimons@gmail.com>
2018-04-08 15:44:35 -0400

    Added music puzzle

--- midi_builder/midi_builder.py
+++ midi_builder/midi_builder.py
@@ -12,7 +12,7 @@ def array_to_str(datatype, name, values):
     return text
 
 
-def build_midi(filename):
+def build_midi(filename, name):
     t = 0
     freqs =[]
     starts = []
@@ -24,7 +24,7 @@ def build_midi(filename):
             note = msg.note
             freq = round(27.5 * 2 ** ((note - 21) / 12))
             velocity = msg.velocity
-            print(t, freq, msg)
+            # print(t, freq, msg)
 
             if velocity == 0:
                 stops.append(int(t * 1000))
@@ -34,10 +34,18 @@ def build_midi(filename):
 
     assert(len(freqs) == len(starts) == len(stops))
 
-    print(array_to_str("int", "s1_freqs", freqs))
-    print(array_to_str("int", "s1_starts", starts))
-    print(array_to_str("int", "s1_stops", stops))
+    t0 = starts[0]
+    starts = [time - t0 for time in starts]
+    stops = [time - t0 for time in stops]
+
+    print(array_to_str("int", "{}_freqs".format(name), freqs))
+    print(array_to_str("int", "{}_starts".format(name), starts))
+    print(array_to_str("int", "{}_stops".format(name), stops))
 
 
 if __name__ == '__main__':
-    build_midi("spider.mid")
+    build_midi("spider1.mid", "spider1")
+    build_midi("spider2.mid", "spider2")
+    build_midi("spider3.mid", "spider3")
+    build_midi("spider4.mid", "spider4")
+    build_midi("spider5.mid", "spider5")
